a0c7cb2279fd4dae3b3da9b324a0db00fb3b5f0b
Martin Thaller <thaller.martin@gmail.com>
2021-12-02 13:12:18 +0100

    template

--- test/AdventOfCode2021.Tests/Day2Tests.fs
+++ test/AdventOfCode2021.Tests/Day2Tests.fs
@@ -3,18 +3,22 @@
 open AdventOfCode2021.Solver
 open Xunit
 
+type Day2Test() =
+    let demoData =
+        [| "forward 5"
+           "down 5"
+           "forward 8"
+           "up 3"
+           "down 8"
+           "forward 2" |]
+
+    [<Fact>]
+    let ``Day 2 part 1`` () =
+        let solution = Day2.solver1 demoData
+        Assert.Equal("150", solution)
 
 
-[<Fact>]
-let ``Day 2 part 1`` () =
-    let demoData = [|"forward 5";"down 5";"forward 8";"up 3";"down 8";"forward 2"|]
-    let solution = Day2.solver1 demoData
-    Assert.Equal ("150", solution)
-
-
-[<Fact>]
-let ``Day 2 part 2`` () =
-    let demoData = [|"forward 5";"down 5";"forward 8";"up 3";"down 8";"forward 2"|]
-    let solution = Day2.solver2 demoData
-    Assert.Equal ("900", solution)
-
+    [<Fact>]
+    let ``Day 2 part 2`` () =
+        let solution = Day2.solver2 demoData
+        Assert.Equal("900", solution)
---
+++ test/AdventOfCode2021.Tests/DayXYTests.fs
@@ -0,0 +1,20 @@
+﻿module AdventOfCode2021.Tests.DayXYTests
+
+open AdventOfCode2021.Solver
+open Xunit
+
+type DayXYTest() =
+    let demoData =
+        [| ""
+           "" |]
+
+    [<Fact(Skip="Template")>]
+    let ``Day XY part 1`` () =
+        let solution = DayXY.solver1 demoData
+        Assert.Equal("", solution)
+
+
+    [<Fact(Skip="Template")>]
+    let ``Day XY part 2`` () =
+        let solution = DayXY.solver2 demoData
+        Assert.Equal("", solution)
