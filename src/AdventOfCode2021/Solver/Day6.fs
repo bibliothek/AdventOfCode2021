071587341c6968550d46b330e34d2d3e35ff4313
Martin Thaller <thaller.martin@gmail.com>
2021-12-06 11:43:48 +0100

    day 6 part 2
    Signed-off-by: Martin Thaller <thaller.martin@gmail.com>

--- src/AdventOfCode2021/Solver/Day6.fs
+++ src/AdventOfCode2021/Solver/Day6.fs
@@ -1,29 +1,26 @@
 ﻿module AdventOfCode2021.Solver.Day6
 
-let updateFish (fish: int) =
-    match fish with
-    | 0 -> 6
-    | i -> i - 1
-
-let rec tick (fish: int[]) idx n =
-    if idx = n then fish
+let rec tick (population: int64 list) idx n : int64 list =
+    if idx = n then population
     else
-        printfn $"%i{idx}"
-        let procreatingFishCount = fish |> Array.filter (fun x -> x = 0) |> Array.length
-        let procreatingFish = [| for _ in 0 .. procreatingFishCount - 1 -> 8 |]
-        let updatedFish = fish |> Array.map updateFish |> Array.append procreatingFish
-        tick updatedFish (idx + 1) n
+        let procreatingFishCount = population.[0]
+        let updatedPopulation = (population |> List.skip 1) @ [procreatingFishCount]
+        let updatedFish6Count = updatedPopulation.[6] + procreatingFishCount
+        let completeUpdatedPopulation = (updatedPopulation |> List.take 6) @ [updatedFish6Count] @ (updatedPopulation |> List.skip 7)
+        tick completeUpdatedPopulation (idx + 1) n
 
 
 
 let solver1 (lines: string array) =
     let fish = lines.[0].Split ',' |> Array.map int
-    let updatedFish = tick fish 0 80
-    updatedFish.Length |> string
+    let population = [ for i in 0 .. 8 -> fish |> Array.filter (fun x-> x = i) |> Array.length |> int64 ]
+    let updatedPopulation = tick population 0 80
+
+    updatedPopulation |> List.sum |> string
 
 let solver2 (lines: string array) =
     let fish = lines.[0].Split ',' |> Array.map int
-    let population = [| for i in 0 .. 8 -> fish |> Array.filter (fun x-> x = i) |> Array.length |> int64 |]
+    let population = [ for i in 0 .. 8 -> fish |> Array.filter (fun x-> x = i) |> Array.length |> int64 ]
+    let updatedPopulation = tick population 0 256
 
-    let updatedFish = tick fish 0 256
-    updatedFish.Length |> string
+    updatedPopulation |> List.sum |> string
